424d3f76de8283112d1340833d06982dbe0fa22b
Jhon Lloyd Borres <Lloydjohns@users.noreply.github.com>
2026-07-18 17:48:10 +0800

    Initial commit

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'YOUR_PROJECT_URL'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'YOUR_ANON_KEY'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://ehaxelwadigpdhxqqjno.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_91Jsjrll4wtLN9YZNeUNsg_oemOpMMV'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
